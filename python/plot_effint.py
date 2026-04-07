d6deece92cfc5a095507b1b3d757b512dd2195d1
eliasarnqvist <elias.arnqvist@physics.uu.se>
2026-04-07 16:52:13 +0200

    now possible to plot coincidences as well

--- python/plot_effint.py
+++ python/plot_effint.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def plot_singles(metadata_peakfinfo, datacut):
-    fig, ax = plt.subplots(1, 1, figsize=(80/inch_to_mm, 60/inch_to_mm))
+    fig, ax = plt.subplots(1, 1, figsize=(88/inch_to_mm, 60/inch_to_mm))
 
     effint = {}
 
@@ -52,8 +52,6 @@ def plot_singles(metadata_peakfinfo, datacut):
                 effint[E_gamma]["y"].append(y_val)
                 effint[E_gamma]["dy"].append(dy_val)
 
-    print(effint)
-
     # Plot the efficiency
     for l, (key, value) in enumerate(effint.items()):
         E_gamma = key
@@ -79,13 +77,93 @@ def plot_singles(metadata_peakfinfo, datacut):
     legend_title = datacut["plot"]["nuclide"] + str(" (keV)")
     ax.legend(frameon=False, fontsize=8, title=legend_title, title_fontsize=8)
 
+    save_name = "figures/" + datacut["plot"]["save_name"] + "_singles"
     plt.tight_layout(pad = 0.2)
     fig.subplots_adjust(hspace=0, wspace=0)
-    plt.savefig("figures/test.jpg", dpi=300)
+    plt.savefig(save_name + ".jpg", dpi=300)
 
 
-def plot_coincidences():
-    return
+def plot_coincidences(metadata_peakfinfo, datacut):
+    fig, ax = plt.subplots(1, 1, figsize=(88/inch_to_mm, 60/inch_to_mm))
+
+    effint = {}
+
+    for i, (key, value) in enumerate(metadata_peakfinfo.items()):
+
+        # Make sure we only plot the data specified in datacut
+        cut_included = True
+        for j, (key2, value2) in enumerate(datacut.items()):
+            if key2 == "plot":
+                continue
+            elif key2 == "type":
+                this_check = (value[key2] == value2)
+                cut_included *= this_check
+            else:
+                if value2 == "any":
+                    this_check = True
+                else:
+                    this_check = (value["properties"][key2] == value2)
+                cut_included *= this_check
+        
+        if not cut_included:
+            continue
+
+        # Look at the peak data
+        peakdata = value["peakdata"]["coincidences"]
+
+        # What to plot on the x-axis
+        plot_on_x = datacut["plot"]["x_plot"]
+
+        for k in range(len(peakdata["E_gamma1"])):
+            # Data values for this energy
+            x_val = value["properties"][plot_on_x]
+            # Coincident energies
+            E_gamma1 = peakdata["E_gamma1"][k]
+            E_gamma2 = peakdata["E_gamma2"][k]
+            counts = peakdata["counts_a1b2"][k] + peakdata["counts_a2b1"][k]
+            events = value["properties"]["runs"]
+            y_val = counts / events
+            # Use Binomial statistics here instead of Poisson
+            dy_val = np.sqrt(counts * (1 - counts/events)) / events
+
+            if k not in effint.keys():
+                entry = {"x": [x_val], "y": [y_val], "dy": [dy_val], "E_gamma1": E_gamma1, "E_gamma2": E_gamma2}
+                effint[k] = entry
+            else:
+                effint[k]["x"].append(x_val)
+                effint[k]["y"].append(y_val)
+                effint[k]["dy"].append(dy_val)
+
+    # Plot the efficiency
+    for key, value in effint.items():
+        E_gamma1 = value["E_gamma1"]
+        E_gamma2 = value["E_gamma2"]
+        x = value["x"]
+        y = value["y"]
+        dy = value["dy"]
+
+        idx = np.argsort(x)
+        x_sorted = np.array(x)[idx]
+        y_sorted = np.array(y)[idx] * 1e2
+        dy_sorted = np.array(dy)[idx] * 1e2
+
+        # Generally 2 decimals for ENSDF data, but check for your case!
+        line_label = f"{E_gamma1:.2f}, {E_gamma2:.2f}"
+        ax.errorbar(x_sorted, y_sorted, yerr=dy_sorted, fmt=".", ls="-", 
+                    markersize=4, lw=1, capsize=2, capthick=1, 
+                    label=line_label)
+    
+    x_label = datacut["plot"]["x_label"]
+    ax.set_xlabel(x_label)
+    y_label = r"$I_{\gamma \gamma} \, \varepsilon_{\gamma \gamma}$ (%)"
+    ax.set_ylabel(y_label)
+    legend_title = datacut["plot"]["nuclide"] + str(" (keV, keV)")
+    ax.legend(frameon=False, fontsize=8, title=legend_title, title_fontsize=8)
+
+    save_name = "figures/" + datacut["plot"]["save_name"] + "_coincidences"
+    plt.tight_layout(pad = 0.2)
+    fig.subplots_adjust(hspace=0, wspace=0)
+    plt.savefig(save_name + ".jpg", dpi=300)
 
 
 # Parser for adding arguments
@@ -93,12 +171,7 @@ parser = argparse.ArgumentParser(prog="plot_effint",
                                  description="Plot the product of efficiency and intensity",
                                  epilog="Elias Arnqvist, 2026, Uppsala University",
                                  add_help=True)
-# parser.add_argument("-x", type=str, required=True, default="None", help="What to plot on the x-axis")
-# parser.add_argument("-y", type=str, required=True, default="None", help="What to plot on the y-axis")
-parser.add_argument("-Z", type=int, required=True, default=0, help="Atomic number (Z) of radionuclide")
-parser.add_argument("-A", type=int, required=True, default=0, help="Mass number (A) of radionuclide")
 parser.add_argument("-dc", "--datacut", type=str, required=True, default="datacut.json", help="File for only selecting part of the dataset")
-parser.add_argument("-s", "--save_name", type=str, required=False, default="figure_test", help="Filename of saved figure")
 parser.add_argument("-mp", "--metadata_peakinfo", type=str, required=False, default="metadata_peakinfo.json", help="Path to metadata and peakinfo file")
 args = parser.parse_args()
 
@@ -107,28 +180,14 @@ metadata_peakfinfo_filepath = args.metadata_peakinfo
 with open(metadata_peakfinfo_filepath, "r") as f:
     metadata_peakfinfo = json.load(f)
 
-ZAs_metadata = [(value["properties"]["Z"], value["properties"]["A"]) for value in metadata_peakfinfo.values() if value["type"] == "radionuclides"]
-ZAs_metadata = list(set(ZAs_metadata))
-
-Z = args.Z
-A = args.A
-print("Using specified ZA: Z=" + str(Z) + ", A=" + str(A))
-if (Z, A) not in ZAs_metadata:
-    raise Exception("Used Z and A not found in metadata")
-
 # Get the information of how to select what to plot
 datacut_filepath = args.datacut
 with open(datacut_filepath, "r") as f:
     datacut = json.load(f)
-
-print(datacut)
 
 # Plot the efficiency and intensity
 plt.close('all')
 inch_to_mm = 25.4
 
 plot_singles(metadata_peakfinfo, datacut)
-
-
-
-
+plot_coincidences(metadata_peakfinfo, datacut)
