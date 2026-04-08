e633ab467a2b5eee0c475a6530086fb16c1c52ba
eliasarnqvist <elias.arnqvist@physics.uu.se>
2026-04-08 13:18:10 +0200

    background analysis and plotting

---
+++ python/analyze_back.py
@@ -0,0 +1,197 @@
+import numpy as np
+import uproot
+import argparse
+# from numba import jit
+import json
+import re
+# from uncertainties import ufloat
+
+
+def start_analysis(ZAs):
+    metadata_backinfo = {}
+
+    print("Starting analysis: ")
+
+    # Go through the metadata and look for the interesting simulations
+    data_keys = []
+    data_values = []
+    for key, value in metadata.items():
+        if value["type"] != "external_background":
+            continue
+        data_values.append(value)
+        data_keys.append(key)
+    
+    print("\tFound " + str(len(data_keys)) + " files")
+
+    # Now do the analysis
+    for i, (data_key, data_value) in enumerate(zip(data_keys, data_values)):
+        data_filename = data_value["filename"]
+        data_filesize = data_value["file_size"] / (1024**2)
+
+        print("\t\tAnalyzing file " + str(i+1) + " out of " + str(len(data_keys)) + ": " + str(data_filename) + f" ({data_filesize:.2f} mb)")
+
+        backdata = analyze_single_and_coincidence(ZAs, data_filename)
+
+        data_value["backdata"] = backdata
+
+        metadata_backinfo[data_key] = data_value
+    
+    return metadata_backinfo
+
+
+def analyze_single_and_coincidence(ZAs, data_filename):
+    data_filepath = args.data + data_filename
+
+    data_file = uproot.open(data_filepath)
+    tree_detector_a = data_file["E_a_list"]
+    tree_detector_b = data_file["E_b_list"]
+    tree_detector_ab = data_file["E_ab_list"]
+
+    # Single gamma-rays
+    events_single_a = tree_detector_a["energy_a"].array(library="np") * 1e3
+    events_single_b = tree_detector_b["energy_b"].array(library="np") * 1e3
+
+    # Coincidence gamma-rays
+    events_coincidence_a = tree_detector_ab["energy_a"].array(library="np") * 1e3
+    events_coincidence_b = tree_detector_ab["energy_b"].array(library="np") * 1e3
+
+    background_data = {}
+
+    for i, (Z, A) in enumerate(ZAs):
+        print("\t\t\tZ=" + str(Z) + ", A=" + str(A) + " (" + str(i+1) + " out of " + str(len(ZAs)) + ")")
+
+        # Gamma ray energy infromation
+        gammas_single = gammas["Z" + str(Z) + "_A" + str(A)]["singles"]["E_gamma"]
+        gammas_coincidence_1 = gammas["Z" + str(Z) + "_A" + str(A)]["coincidences"]["E_gamma1"]
+        gammas_coincidence_2 = gammas["Z" + str(Z) + "_A" + str(A)]["coincidences"]["E_gamma2"]
+
+        # Single detector counts
+        counts_single_a = []
+        counts_single_b = []
+        for E_gamma in gammas_single:
+            counts_a = count_1d(events_single_a, E_gamma, ROI_standard)
+            counts_single_a.append(float(counts_a))
+            counts_b = count_1d(events_single_b, E_gamma, ROI_standard)
+            counts_single_b.append(float(counts_b))
+
+        # Coincidence counts
+        counts_coincidence_a1b2 = []
+        counts_coincidence_a2b1 = []
+        for E_gamma1, E_gamma2 in zip(gammas_coincidence_1, gammas_coincidence_2):
+            counts_a1b2 = count_2d(events_coincidence_a, events_coincidence_b, E_gamma1, E_gamma2, ROI_standard)
+            counts_coincidence_a1b2.append(float(counts_a1b2))
+            counts_a2b1 = count_2d(events_coincidence_a, events_coincidence_b, E_gamma2, E_gamma1, ROI_standard)
+            counts_coincidence_a2b1.append(float(counts_a2b1))
+
+        ZA_peak_data = {
+            "singles" : {
+                "E_gamma" : gammas_single,
+                "counts_a" : counts_single_a,
+                "counts_b" : counts_single_b,
+                "ROI_type" : "standard",
+                "ROI" : ROI_standard,
+            },
+            "coincidences" : {
+                "E_gamma1" : gammas_coincidence_1,
+                "E_gamma2" : gammas_coincidence_2,
+                "counts_a1b2" : counts_coincidence_a1b2,
+                "counts_a2b1" : counts_coincidence_a2b1,
+                "ROI_type" : "quadruple_square",
+                "ROI" : ROI_standard,
+            },
+        }
+
+        background_data["Z" + str(Z) + "_A" + str(A)] = ZA_peak_data
+
+    return background_data
+
+
+# @jit(nopython=True)
+def count_1d(events, Eg, ROI):
+    # Energy distance is half of the ROI size
+    dE = ROI/2
+
+    # Count peak counts
+    cond = np.logical_and(events > Eg-dE, events < Eg+dE)
+    counts = cond.sum()
+    return counts
+
+
+# def count_2d(events_a, events_b, Eg1, Eg2, ROI):
+#     # Energy distance is half of the ROI size
+#     dE = ROI/2
+
+#     # Count peak counts
+#     cond_1 = np.logical_and(events_a > Eg1-dE, events_a < Eg1+dE)
+#     cond_2 = np.logical_and(events_b > Eg2-dE, events_b < Eg2+dE)
+#     cond = np.logical_and(cond_1, cond_2)
+#     counts = cond.sum()
+#     return counts
+
+
+def count_2d(events_a, events_b, Eg1, Eg2, ROI):
+    # Energy distance is half of the ROI size
+    dE = 4 * ROI/2
+
+    # Count peak counts
+    cond_1 = np.logical_and(events_a > Eg1-dE, events_a < Eg1+dE)
+    cond_2 = np.logical_and(events_b > Eg2-dE, events_b < Eg2+dE)
+    cond = np.logical_and(cond_1, cond_2)
+    counts = cond.sum()
+    counts /= (4**2)
+    return counts
+
+
+# Parser for adding arguments
+parser = argparse.ArgumentParser(prog="analyze_back",
+                                 description="Analyze .root files to determine the background count rate",
+                                 epilog="Elias Arnqvist, 2026, Uppsala University",
+                                 add_help=True)
+parser.add_argument("-Z", type=int, required=False, default=0, help="Atomic number (Z) of radionuclide")
+parser.add_argument("-A", type=int, required=False, default=0, help="Mass number (A) of radionuclide")
+parser.add_argument("-d", "--data", type=str, required=False, default="../geant4/output_background/", help="Path to directory with root files")
+parser.add_argument("-m", "--metadata", type=str, required=False, default="../geant4/output_background/metadata.json", help="Path to metadata file")
+parser.add_argument("-g", "--gammas", type=str, required=False, default="gammas.json", help="Path to gamma ray information")
+parser.add_argument("-o", "--output", type=str, required=False, default="metadata_backinfo.json", help="Path to save output data")
+args = parser.parse_args()
+
+ROI_standard = 5
+
+# Open gamma ray data file
+gammas_filepath = args.gammas
+print("Opening gamma file: " + gammas_filepath)
+with open(gammas_filepath, "r") as f:
+    gammas = json.load(f)
+
+# Open metadata file
+metadata_filepath = args.metadata
+print("Opening metadata file: " + metadata_filepath)
+with open(metadata_filepath, "r") as f:
+    metadata = json.load(f)
+
+# Dictionary to save results in
+metadata_backinfo = {}
+
+# Look for radionuclides in the gammas and metadata
+# Make a list of Z, A present in gammas and metadata
+ZAs_gammas = [(int(m.group(1)), int(m.group(2))) for key in gammas.keys() if (m := re.match(r"Z(\d+)_A(\d+)", key))]
+
+Z = args.Z
+A = args.A
+if Z != 0 and A != 0:
+    # Z and A are specified
+    print("Using specified ZA: Z=" + str(Z) + ", A=" + str(A))
+    if (Z, A) not in ZAs_gammas:
+        raise Exception("Used Z and A not found in gamma ray data")
+
+    metadata_backinfo = start_analysis([(Z, A)])
+else:
+    # Z and A not specified, so use the ones in the gammas
+    print("Using ZA in gamma file: " + str(ZAs_gammas))
+    
+    metadata_backinfo = start_analysis(ZAs_gammas)
+
+output_filepath = args.output
+print("Writing output file: " + output_filepath)
+with open(output_filepath, "w") as f:
+    json.dump(metadata_backinfo, f, indent=4)
---
+++ python/plot_back.py
@@ -0,0 +1,193 @@
+import argparse
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+
+
+def plot_singles(metadata_peakfinfo, datacut):
+    fig, ax = plt.subplots(1, 1, figsize=(88/inch_to_mm, 60/inch_to_mm))
+
+    effint = {}
+
+    for i, (key, value) in enumerate(metadata_peakfinfo.items()):
+
+        # Make sure we only plot the data specified in datacut
+        cut_included = True
+        for j, (key2, value2) in enumerate(datacut.items()):
+            if key2 == "plot":
+                continue
+            elif key2 == "type":
+                this_check = (value[key2] == value2)
+                cut_included *= this_check
+            else:
+                if value2 == "any":
+                    this_check = True
+                else:
+                    this_check = (value["properties"][key2] == value2)
+                cut_included *= this_check
+        
+        if not cut_included:
+            continue
+
+        # Look at the peak data
+        peakdata = value["peakdata"]["singles"]
+
+        # What to plot on the x-axis
+        plot_on_x = datacut["plot"]["x_plot"]
+
+        for k, E_gamma in enumerate(peakdata["E_gamma"]):
+            # Data values for this energy
+            x_val = value["properties"][plot_on_x]
+            counts = peakdata["counts_a"][k] + peakdata["counts_b"][k]
+            events = value["properties"]["events"]
+            y_val = counts / events
+            # Use Binomial statistics here instead of Poisson
+            dy_val = np.sqrt(counts * (1 - counts/events)) / events
+
+            if E_gamma not in effint.keys():
+                entry = {"x": [x_val], "y": [y_val], "dy": [dy_val]}
+                effint[E_gamma] = entry
+            else:
+                effint[E_gamma]["x"].append(x_val)
+                effint[E_gamma]["y"].append(y_val)
+                effint[E_gamma]["dy"].append(dy_val)
+
+    # Plot the efficiency
+    for l, (key, value) in enumerate(effint.items()):
+        E_gamma = key
+        x = value["x"]
+        y = value["y"]
+        dy = value["dy"]
+
+        idx = np.argsort(x)
+        x_sorted = np.array(x)[idx]
+        y_sorted = np.array(y)[idx] * 1e2
+        dy_sorted = np.array(dy)[idx] * 1e2
+
+        # Generally 2 decimals for ENSDF data, but check for your case!
+        line_label = f"{E_gamma:.2f}"
+        ax.errorbar(x_sorted, y_sorted, yerr=dy_sorted, fmt=".", ls="-", 
+                    markersize=4, lw=1, capsize=2, capthick=1, 
+                    label=line_label)
+    
+    x_label = datacut["plot"]["x_label"]
+    ax.set_xlabel(x_label)
+    y_label = r"$I_\gamma \, \varepsilon_\gamma$ (%)"
+    ax.set_ylabel(y_label)
+    legend_title = datacut["plot"]["nuclide"] + str(" (keV)")
+    ax.legend(frameon=False, fontsize=8, title=legend_title, title_fontsize=8)
+
+    save_name = "figures/" + datacut["plot"]["save_name"] + "_singles"
+    plt.tight_layout(pad = 0.2)
+    fig.subplots_adjust(hspace=0, wspace=0)
+    plt.savefig(save_name + ".jpg", dpi=300)
+
+
+def plot_coincidences(metadata_peakfinfo, datacut):
+    fig, ax = plt.subplots(1, 1, figsize=(88/inch_to_mm, 60/inch_to_mm))
+
+    effint = {}
+
+    for i, (key, value) in enumerate(metadata_peakfinfo.items()):
+
+        # Make sure we only plot the data specified in datacut
+        cut_included = True
+        for j, (key2, value2) in enumerate(datacut.items()):
+            if key2 == "plot":
+                continue
+            elif key2 == "type":
+                this_check = (value[key2] == value2)
+                cut_included *= this_check
+            else:
+                if value2 == "any":
+                    this_check = True
+                else:
+                    this_check = (value["properties"][key2] == value2)
+                cut_included *= this_check
+        
+        if not cut_included:
+            continue
+
+        # Look at the peak data
+        peakdata = value["peakdata"]["coincidences"]
+
+        # What to plot on the x-axis
+        plot_on_x = datacut["plot"]["x_plot"]
+
+        for k in range(len(peakdata["E_gamma1"])):
+            # Data values for this energy
+            x_val = value["properties"][plot_on_x]
+            # Coincident energies
+            E_gamma1 = peakdata["E_gamma1"][k]
+            E_gamma2 = peakdata["E_gamma2"][k]
+            counts = peakdata["counts_a1b2"][k] + peakdata["counts_a2b1"][k]
+            events = value["properties"]["events"]
+            y_val = counts / events
+            # Use Binomial statistics here instead of Poisson
+            dy_val = np.sqrt(counts * (1 - counts/events)) / events
+
+            if k not in effint.keys():
+                entry = {"x": [x_val], "y": [y_val], "dy": [dy_val], "E_gamma1": E_gamma1, "E_gamma2": E_gamma2}
+                effint[k] = entry
+            else:
+                effint[k]["x"].append(x_val)
+                effint[k]["y"].append(y_val)
+                effint[k]["dy"].append(dy_val)
+
+    # Plot the efficiency
+    for key, value in effint.items():
+        E_gamma1 = value["E_gamma1"]
+        E_gamma2 = value["E_gamma2"]
+        x = value["x"]
+        y = value["y"]
+        dy = value["dy"]
+
+        idx = np.argsort(x)
+        x_sorted = np.array(x)[idx]
+        y_sorted = np.array(y)[idx] * 1e2
+        dy_sorted = np.array(dy)[idx] * 1e2
+
+        # Generally 2 decimals for ENSDF data, but check for your case!
+        line_label = f"{E_gamma1:.2f}, {E_gamma2:.2f}"
+        ax.errorbar(x_sorted, y_sorted, yerr=dy_sorted, fmt=".", ls="-", 
+                    markersize=4, lw=1, capsize=2, capthick=1, 
+                    label=line_label)
+    
+    x_label = datacut["plot"]["x_label"]
+    ax.set_xlabel(x_label)
+    y_label = r"$I_{\gamma \gamma} \, \varepsilon_{\gamma \gamma}$ (%)"
+    ax.set_ylabel(y_label)
+    legend_title = datacut["plot"]["nuclide"] + str(" (keV, keV)")
+    ax.legend(frameon=False, fontsize=8, title=legend_title, title_fontsize=8)
+
+    save_name = "figures/" + datacut["plot"]["save_name"] + "_coincidences"
+    plt.tight_layout(pad = 0.2)
+    fig.subplots_adjust(hspace=0, wspace=0)
+    plt.savefig(save_name + ".jpg", dpi=300)
+
+
+# Parser for adding arguments
+parser = argparse.ArgumentParser(prog="plot_effint",
+                                 description="Plot the product of efficiency and intensity",
+                                 epilog="Elias Arnqvist, 2026, Uppsala University",
+                                 add_help=True)
+parser.add_argument("-dc", "--datacut", type=str, required=True, default="datacut.json", help="File for only selecting part of the dataset")
+parser.add_argument("-mp", "--metadata_peakinfo", type=str, required=False, default="metadata_peakinfo.json", help="Path to metadata and peakinfo file")
+args = parser.parse_args()
+
+# Get the metadata and peak counts
+metadata_peakfinfo_filepath = args.metadata_peakinfo
+with open(metadata_peakfinfo_filepath, "r") as f:
+    metadata_peakfinfo = json.load(f)
+
+# Get the information of how to select what to plot
+datacut_filepath = args.datacut
+with open(datacut_filepath, "r") as f:
+    datacut = json.load(f)
+
+# Plot the efficiency and intensity
+plt.close('all')
+inch_to_mm = 25.4
+
+plot_singles(metadata_peakfinfo, datacut)
+plot_coincidences(metadata_peakfinfo, datacut)
