18f4a42bb420d39661e0177d55739b267760ce13
eliasarnqvist <elias.arnqvist@physics.uu.se>
2026-07-10 10:11:14 +0200

    added plotting to mda plot script

--- python/plot_mda.py
+++ python/plot_mda.py
@@ -1,12 +1,14 @@
 import argparse
 import json
-import time
-import subprocess
-import uuid
-import os
+# import time
+# import subprocess
+# import uuid
+# import os
 import numpy as np
 import uproot
 import pprint
+import matplotlib.pyplot as plt
+
 # from analysis.cut_metadata import cut_metadata
 
 
@@ -132,9 +134,9 @@ def analyze_files(data_list, plotcard, metadata, data_path):
     # Need to iterate through the relevant files
     for i, (key, value) in enumerate(metadata.items()):
 
-        # NOTE just for testing
-        if i > 5:
-            continue
+        # # NOTE just for testing
+        # if i > 9:
+        #     continue
 
         run_type = value["type"]
         data_filename = value["filename"]
@@ -475,15 +477,43 @@ def calculate_combined_mda(mda_list):
     return float(mda_combined), float(mda_combined_unc)
 
 
-def plot_mda(plotcard):
-    metadata = plotcard[""]
+def plot_mda(data_list, plotcard):
+    plot_settings = plotcard["plot"]
+    x_key = plot_settings["x_key"]
+    x_label = plot_settings["x_label"]
+    y_key = plot_settings["y_key"]
+    y_label = plot_settings["y_label"]
+    legend_title = plot_settings["legend_title"]
 
-    # Now the MDA can be caluclated in the next step
-    return
+    # fig, ax = plt.subplots(3, 1, figsize=(88/inch_to_mm, 130/inch_to_mm), sharex=True, gridspec_kw={"height_ratios": [2, 1, 1]})
+    fig, ax = plt.subplots(1, 1, figsize=(88/inch_to_mm, 60/inch_to_mm))
+
+    for line_setting in plot_settings["lines"]:
+        line_label = line_setting.pop("plot_label")
+        data_points = []
+        
+        for data_point in data_list:
+            if all(data_point[k] == v for k, v in line_setting.items()):
+                # data_point["counts_radionuclide"] = counts
+                data_points.append(data_point)
+
+        sort_order = sorted(range(len(data_points)), key=lambda i: data_points[i][x_key])
+        x_values = [data_points[i][x_key] for i in sort_order]
+        y_values = [data_points[i][y_key][0] for i in sort_order]
+        y_values_unc = [data_points[i][y_key][1] for i in sort_order]
+
+        ax.errorbar(x_values, y_values, yerr=y_values_unc, fmt=".", ls="-", 
+                       markersize=4, lw=1, capsize=2, capthick=1, 
+                       label=line_label)
+
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    
+    ax.legend(frameon=False, fontsize=8, title=legend_title, title_fontsize=8)
+
+    plt.show()
 
 
-def make_plot():
-    return
 
 
 # Parser for adding arguments
@@ -515,7 +545,12 @@ data_list = create_data_list(plotcard, metadata)
 
 data_list = analyze_files(data_list, plotcard, metadata, data_path)
 
-pprint.pp(data_list)
+# pprint.pp(data_list)
+
+# Plot the MDA
+plt.close('all')
+inch_to_mm = 25.4
+plot_mda(data_list, plotcard)
 
 # for data_point in data_list:
 #     print(data_point["effint"])
