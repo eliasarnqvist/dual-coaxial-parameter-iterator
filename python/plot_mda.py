1f387ac126a6f8780578128b1fbb5beb54f624be
eliasarnqvist <elias.arnqvist@physics.uu.se>
2026-07-10 16:50:00 +0200

    takes into consideration the filter background now

--- python/plot_mda.py
+++ python/plot_mda.py
@@ -41,8 +41,8 @@ def cut_metadata(plotcard, metadata):
         elif value["type"] == "filter":
             ZA = [value["properties"]["Z"], value["properties"]["A"]]
             ZAs_filter = [plotcard["filter"]["ZAs_Bq_filter"][i][0:2] for i in range(len(plotcard["filter"]["ZAs_Bq_filter"]))]
-            ZAs_exposure = [plotcard["filter"]["ZAs_Bq_filter"][i][0:2] for i in range(len(plotcard["filter"]["ZAs_Bq_filter"]))]
-            if (ZA not in ZAs_filter) and (ZA not in ZAs_exposure):
+            filter_active = plotcard["filter"]["filter_active"]
+            if (ZA not in ZAs_filter) or (True not in filter_active):
                 keep_this_key = False
         else:
             raise ValueError
@@ -122,20 +122,33 @@ def create_data_list(plotcard, metadata):
                 pass
     data_list = new_data_list
 
+    # Next use the plotcard to supply the filter background information
+    new_data_list = []
+    filter_actives = plotcard["filter"]["filter_active"]
+    for data_point in data_list:
+        # Need a new data point for the filter activity options
+        for filter_active in filter_actives:
+            new_data_point = data_point.copy() # important with .copy() here!
+            new_data_point["filter_active"] = filter_active
+            new_data_list.append(new_data_point)
+    data_list = new_data_list
+
     return data_list
 
 
+# TODO determine optimal measurement time for lowest mda
 def analyze_files(data_list, plotcard, metadata, data_path):
     print("Starting analysis of data files!")
 
     # Quantities not needed inside a loop
+    # TODO Optional determination of the measurement time for optimal mda
     measurement_time_hours = plotcard["analysis"]["measurement_time_hours"]
 
     # Need to iterate through the relevant files
     for i, (key, value) in enumerate(metadata.items()):
 
         # # NOTE just for testing
-        # if i > 9:
+        # if i > 4:
         #     continue
 
         run_type = value["type"]
@@ -273,9 +286,16 @@ def analyze_files(data_list, plotcard, metadata, data_path):
                         events = value["properties"]["events"]
                         pseudo_time = value["properties"]["SURE_pseudo_time"]
 
-                        # Caluclate the LD
-                        LD, LD_unc = calculate_LD(counts, events, pseudo_time, measurement_time_hours)
-
+                        # Calculate the number of background counts
+                        # Convert to seconds
+                        measurement_time = measurement_time_hours * 3600
+                        # Background count rate
+                        b = counts / pseudo_time
+                        b_unc = np.sqrt(counts * (1 - counts/events)) / pseudo_time
+                        # Background counts during measurement time
+                        B = b * measurement_time
+                        B_unc = b_unc * measurement_time
+                    
                     # Need to make a copy below so the keys for singles do not interfere with the keys for coincidences
                     this_data_point_criteria = data_point_criteria.copy()
                     this_data_point_criteria["analysis_type"] = "singles"
@@ -285,7 +305,7 @@ def analyze_files(data_list, plotcard, metadata, data_path):
                     for data_point in data_list:
                         if all(data_point[k] == v for k, v in this_data_point_criteria.items()):
                             # data_point["counts_radionuclide"] = counts
-                            data_point["LD"] = [LD, LD_unc]
+                            data_point["B"] = [B, B_unc]
                     
                 # Analyze coincidence gamma rays also
                 for Egamma1_Egamma2 in gammas_coincidences:
@@ -307,8 +327,15 @@ def analyze_files(data_list, plotcard, metadata, data_path):
                         events = value["properties"]["events"]
                         pseudo_time = value["properties"]["SURE_pseudo_time"]
 
-                        # Caluclate the LD
-                        LD, LD_unc = calculate_LD(counts, events, pseudo_time, measurement_time_hours)
+                        # Calculate the number of background counts
+                        # Convert to seconds
+                        measurement_time = measurement_time_hours * 3600
+                        # Background count rate
+                        b = counts / pseudo_time
+                        b_unc = np.sqrt(counts * (1 - counts/events)) / pseudo_time
+                        # Background counts during measurement time
+                        B = b * measurement_time
+                        B_unc = b_unc * measurement_time
 
                     # Need to make a copy below so the keys for singles do not interfere with the keys for coincidences
                     this_data_point_criteria = data_point_criteria.copy()
@@ -319,13 +346,139 @@ def analyze_files(data_list, plotcard, metadata, data_path):
                     for data_point in data_list:
                         if all(data_point[k] == v for k, v in this_data_point_criteria.items()):
                             # data_point["counts_radionuclide"] = counts
-                            data_point["LD"] = [LD, LD_unc]
+                            data_point["B"] = [B, B_unc]
 
         elif run_type == "filter":
-            # Do this later
-            pass
+            # A criteria of applicability is as follows
+            data_point_criteria["filter_active"] = True
+            # Identify the ZA of the simulation
+            filter_ZA = [value["properties"]["Z"], value["properties"]["A"]]
+
+            # Go through the radionuclides and analyze the filter contribution
+            for j, ZA in enumerate(plotcard["radionuclides"]["ZAs"]):
+                print("\t\tRadionuclide " + str(j+1) + " out of " + str(len(plotcard["radionuclides"]["ZAs"])) + f": Z={ZA[0]}, A={ZA[1]}")
+
+                data_point_criteria["ZA"] = ZA
+
+                # Check which energies are relevant for this radionuclide
+                gammas_singles = plotcard["radionuclides"]["gammas_singles"][j]
+                gammas_coincidences = plotcard["radionuclides"]["gammas_coincidences"][j]
+
+                # Analyze single gamma rays
+                for E_gamma in gammas_singles:
+                    if E_gamma == "combined":
+                        # Handle this later
+                        continue
+                    else:
+                        ROI_polynial_coeffs = plotcard["analysis"]["ROI_width_polynomial"]
+                        ROI_width = np.polyval(ROI_polynial_coeffs, E_gamma)
+
+                        counts_a = ROI_analysis_1D(events_single_a, E_gamma, ROI_width)
+                        counts_b = ROI_analysis_1D(events_single_b, E_gamma, ROI_width)
+                        print("\t\t\tGamma ray: " + str(E_gamma) + " keV, counts a: " + str(counts_a) + ", counts b: " + str(counts_b))
+
+                        counts = counts_a + counts_b
+                        events = value["properties"]["events"]
+                        ZA_Bq_filter = next((x for x in plotcard["filter"]["ZAs_Bq_filter"] if x[:2] == filter_ZA), None)
+                        Bq = ZA_Bq_filter[2]
+                        Bq_unc = ZA_Bq_filter[3]
+
+                        # Calculate the number of background counts
+                        # Convert to seconds
+                        measurement_time = measurement_time_hours * 3600
+                        # Calculate efficiency and intensity
+                        effint = counts / events
+                        # Use binomial instead of poisson approximation
+                        effint_unc = np.sqrt(counts * (1 - counts/events)) / events
+                        # Background counts
+                        B_filter = effint * Bq * measurement_time
+                        B_filter_unc = np.sqrt(np.power(Bq*measurement_time*effint_unc, 2) + np.power(effint*measurement_time*Bq_unc, 2))
+                    
+                    # Need to make a copy below so the keys for singles do not interfere with the keys for coincidences
+                    this_data_point_criteria = data_point_criteria.copy()
+                    this_data_point_criteria["analysis_type"] = "singles"
+                    this_data_point_criteria["gamma"] = E_gamma
+                    
+                    # Store the data in the data_list
+                    for data_point in data_list:
+                        if all(data_point[k] == v for k, v in this_data_point_criteria.items()):
+                            if "B_filter" in data_point:
+                                new_B_filter = data_point["B_filter"][0] + B_filter
+                                new_B_filter_unc = np.sqrt(np.power(data_point["B_filter"][1], 2) + B_filter_unc)
+                                data_point["B_filter"] = [new_B_filter, new_B_filter_unc]
+                            else:
+                                data_point["B_filter"] = [B_filter, B_filter_unc]
+                
+                # Analyze single gamma rays
+                # Analyze coincidence gamma rays also
+                for Egamma1_Egamma2 in gammas_coincidences:
+                    if Egamma1_Egamma2 == "combined":
+                        # Handle this later
+                        continue
+                    else:
+                        E_gamma1, E_gamma2 = Egamma1_Egamma2
+
+                        ROI_polynial_coeffs = plotcard["analysis"]["ROI_width_polynomial"]
+                        ROI_width1 = np.polyval(ROI_polynial_coeffs, E_gamma1)
+                        ROI_width2 = np.polyval(ROI_polynial_coeffs, E_gamma2)
+
+                        counts_a1b2 = ROI_analysis_2D(events_coincidence_a, events_coincidence_b, E_gamma1, E_gamma2, ROI_width1, ROI_width2)
+                        counts_b1a2 = ROI_analysis_2D(events_coincidence_b, events_coincidence_a, E_gamma1, E_gamma2, ROI_width1, ROI_width2)
+                        print("\t\t\tGamma ray 1: " + str(E_gamma1) + " keV, gamma ray 2: " + str(E_gamma2) + " keV , counts a1b2: " + str(counts_a1b2) + ", counts b2a1: " + str(counts_b1a2))
+
+                        counts = counts_a1b2 + counts_b1a2
+                        events = value["properties"]["events"]
+                        ZA_Bq_filter = next((x for x in plotcard["filter"]["ZAs_Bq_filter"] if x[:2] == filter_ZA), None)
+                        Bq = ZA_Bq_filter[2]
+                        Bq_unc = ZA_Bq_filter[3]
+
+                        # Calculate the number of background counts
+                        # Convert to seconds
+                        measurement_time = measurement_time_hours * 3600
+                        # Calculate efficiency and intensity
+                        effint = counts / events
+                        # Use binomial instead of poisson approximation
+                        effint_unc = np.sqrt(counts * (1 - counts/events)) / events
+                        # Background counts
+                        B_filter = effint * Bq * measurement_time
+                        B_filter_unc = np.sqrt(np.power(Bq*measurement_time*effint_unc, 2) + np.power(effint*measurement_time*Bq_unc, 2))
+                    
+                    # Need to make a copy below so the keys for singles do not interfere with the keys for coincidences
+                    this_data_point_criteria = data_point_criteria.copy()
+                    this_data_point_criteria["analysis_type"] = "coincidences"
+                    this_data_point_criteria["gamma"] = Egamma1_Egamma2
+                    
+                    # Store the data in the data_list
+                    for data_point in data_list:
+                        if all(data_point[k] == v for k, v in this_data_point_criteria.items()):
+                            if "B_filter" in data_point:
+                                new_B_filter = data_point["B_filter"][0] + B_filter
+                                new_B_filter_unc = np.sqrt(np.power(data_point["B_filter"][1], 2) + B_filter_unc)
+                                data_point["B_filter"] = [new_B_filter, new_B_filter_unc]
+                            else:
+                                data_point["B_filter"] = [B_filter, B_filter_unc]
         else:
             pass
+
+    # Now we can calculate the detection limit for the data points
+    for data_point in data_list:
+        if data_point["gamma"] == "combined":
+                # Handle this later
+                continue
+
+        # Check if background and/or filter background is present for this data point
+        B_tot = 0
+        B_tot_unc = 0
+        if ("B" in data_point):
+            B_tot += data_point["B"][0]
+            B_tot_unc = np.sqrt(np.power(B_tot_unc, 2) + np.power(data_point["B"][1], 2))
+        if ("B_filter" in data_point):
+            B_tot += data_point["B_filter"][0]
+            B_tot_unc = np.sqrt(np.power(B_tot_unc, 2) + np.power(data_point["B_filter"][1], 2))
+
+        LD, LD_unc = calculate_LD([B_tot, B_tot_unc])
+
+        data_point["LD"] = [LD, LD_unc]
 
     # Now it is possible to caluclate the MDA for each data point
     for data_point in data_list:
@@ -364,6 +517,7 @@ def analyze_files(data_list, plotcard, metadata, data_path):
             "ZA": data_point["ZA"],
             "background_file": data_point["background_file"],
             "analysis_type": data_point["analysis_type"],
+            "filter_active": data_point["filter_active"],
         }
         for data_point_candidate in data_list:
             if all(data_point_candidate[k] == v for k, v in data_point_criteria.items()) and data_point_candidate["gamma"] != "combined":
@@ -413,22 +567,16 @@ def caluclate_effint(counts, events):
 
 # TODO implement proper detection limit formula that works for low counts
 # TODO implement uncertainty of B when B is zero (one-sided uncertainty)
-def calculate_LD(counts, events, pseudo_time, measurement_time_hours):
-    # Convert to seconds
-    measurement_time = measurement_time_hours * 3600
-    # Background count rate
-    b = counts / pseudo_time
-    # Background counts during measurement time
-    B = b * measurement_time
+def calculate_LD(B_):
+    # Background counts
+    B, B_unc = B_
+
     if B == 0:
         print("Encountered 0 background!!!")
         raise ValueError
     # Detection limit according to Currie
     LD = 2.71 + 4.65*np.sqrt(B)
-
     # Calculate the uncertainty
-    b_unc = np.sqrt(counts * (1 - counts/events)) / pseudo_time
-    B_unc = b_unc * measurement_time
     LD_unc = 4.65 * (0.5/np.sqrt(B)) * B_unc
 
     return float(LD), float(LD_unc)
@@ -441,9 +589,6 @@ def calculate_mda(LD_, effint_, measurement_time_hours, t12=0):
     # Extract uncertainties
     LD, LD_unc = LD_
     effint, effint_unc = effint_
-
-    # Optional determination of the measurement time for optimal mda
-    # TODO here
     
     # Calculate the minimum detectable activity
     mda = LD / (effint * tM)
@@ -545,12 +690,16 @@ data_list = create_data_list(plotcard, metadata)
 
 data_list = analyze_files(data_list, plotcard, metadata, data_path)
 
-# pprint.pp(data_list)
+pprint.pp(data_list)
 
 # Plot the MDA
 plt.close('all')
 inch_to_mm = 25.4
-plot_mda(data_list, plotcard)
+# plot_mda(data_list, plotcard)
+
+
+
+
 
 # for data_point in data_list:
 #     print(data_point["effint"])
